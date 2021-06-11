04943d0b6ddd5a2494e7b096a4c35275093abcfd
Labden <nedbal.trent@gmail.com>
2021-06-11 11:34:43 -0400

    LibraryTerminal

---
+++ LibraryTerminal/Library.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryTerminal
+{
+    class Library
+    {
+        public List<Books> Books { get; set; } = new List<Books>();
+
+        public Library()
+        {
+            List<string> shrekScenes = new List<string>() {};
+            Books.Add(new Books());
+
+            List<string> silentHillScenes = new List<string>() {};
+            Books.Add(new Books());
+
+            List<string> theNotebookScenes = new List<string>() {};
+            Books.Add(new Books());
+
+            List<string> pulpFictionScenes = new List<string>() {};
+            Books.Add(new Books());
+
+            List<string> fightClubScenes = new List<string>() {};
+            Books.Add(new Books());
+
+            List<string> pineappleExpressScenes = new List<string>() {};
+            Books.Add(new Books());
+        }
+    }
+}
